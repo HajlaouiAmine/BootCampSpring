58c6ea252ab9970b12255844b6f8c10048454e87
HajlaouiAmine <amine.hajlaoui.77@gmail.com>
2022-02-06 16:18:19 +0100

    add de css

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -1,13 +1,27 @@
 package com.sip.ams.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	@RequestMapping("/info")
-	public String info() 
+	public String info(Model model) 
 	{
+		String formation = "Fullstack";
+		
+		System.out.println("methode info");
+		
+		ArrayList<String> names = new ArrayList<>();
+		names.add("OCA");
+		names.add("OCP");
+		names.add("Spring");
+		names.add("Angular");
+		model.addAttribute("names",names);
+		model.addAttribute("workshop",formation);
 		return "home/info";
 	}
 	@RequestMapping("/affichage")
